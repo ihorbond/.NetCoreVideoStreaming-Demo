9595dd05f3edbeb0d1ca2158e96bdb0dd17882e0
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2019-05-02 17:56:49 -0400

    Add project files.

---
+++ VideoStreaming/Services/IStreamingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoStreaming.Services
+{
+    public interface IStreamingService
+    {
+        Task<Stream> GetVideoByName(string name);
+
+    }
+}
---
+++ VideoStreaming/Services/StreamingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoStreaming.Services
+{
+    public class StreamingService : IStreamingService
+    {
+        public Task<Stream> GetVideoByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
