f7cbeac2007d07eaaeb44f4bcc076c94f5745280
Ihor Bodnarchuk <Ihor.Bodnarchuk@fsresidential.com>
2019-05-02 22:19:18 -0400

    add exception handling middleware

---
+++ VideoStreaming/Models/ApiError.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VideoStreaming.Models
+{
+    public class ApiError
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+}
--- VideoStreaming/Startup.cs
+++ VideoStreaming/Startup.cs
@@ -4,7 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -12,6 +14,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using VideoStreaming.Models;
 using VideoStreaming.Services;
 
 namespace VideoStreaming
@@ -45,6 +49,29 @@ namespace VideoStreaming
             }
             else
             {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context => {
+                        var ex = context.Features.Get<IExceptionHandlerPathFeature>();
+                        if (ex?.Error is NullReferenceException)
+                            context.Response.StatusCode = 404;
+                        else if (ex?.Error is InvalidOperationException)
+                            context.Response.StatusCode = 400;
+                        else
+                            context.Response.StatusCode = 500;
+
+                        context.Response.ContentType = "application/json";
+
+                        ApiError error = new ApiError()
+                        {
+                            Code = context.Response.StatusCode,
+                            Message = env.IsDevelopment() ? ex?.Error.Message : "An unexpected error happened. Try again later."
+                        };
+
+                        await context.Response.WriteAsync(JsonConvert.SerializeObject(error)).ConfigureAwait(false);
+                    });
+                });
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
